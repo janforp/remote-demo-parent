f00ad0cecee8a38715fb2a7cd9af287251032987
Janita <zhuchenjian@huizhi-tech.com>
2017-03-23 09:09:15 +0800

    Initial commit

---
+++ remote-one/src/main/java/com/janita/remote/one/RemoteOneApplication.java
@@ -0,0 +1,16 @@
+package com.janita.remote.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017-03-23 09:06
+ */
+@SpringBootApplication
+public class RemoteOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteOneApplication.class, args);
+    }
+
+}
